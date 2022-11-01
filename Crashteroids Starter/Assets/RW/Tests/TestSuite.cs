30f1e930bcf7a7633f6e0cb1f44e11e046f0b190
17-WynnOng <Wynnong12@gmail.com>
2022-11-01 11:05:09 +0800

    Test Batch 1

--- Crashteroids Starter/Assets/RW/Tests/TestSuite.cs
+++ Crashteroids Starter/Assets/RW/Tests/TestSuite.cs
@@ -6,23 +6,42 @@ using UnityEngine.TestTools;
 
 namespace Tests
 {
-    public class TestSuite
-    {
-        // A Test behaves as an ordinary method
-        [Test]
-        public void NewTestScriptSimplePasses()
+        public class TestSuite
         {
-            // Use the Assert class to test conditions
-        }
+            private Game game;
 
-        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-        // `yield return null;` to skip a frame.
-        [UnityTest]
-        public IEnumerator NewTestScriptWithEnumeratorPasses()
-        {
-            // Use the Assert class to test conditions.
-            // Use yield to skip a frame.
-            yield return null;
+            [SetUp]
+            public void Setup()
+            {
+                GameObject gameGameObject =
+                    MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Game"));
+                game = gameGameObject.GetComponent<Game>();
+            }
+
+            [TearDown]
+            public void Teardown()
+            {
+                Object.Destroy(game.gameObject);
+            }
+
+            [UnityTest]
+            public IEnumerator AsteroidsMoveDown()
+            {
+                GameObject asteroid = game.GetSpawner().SpawnAsteroid();
+                float initialYPos = asteroid.transform.position.y;
+                yield return new WaitForSeconds(0.1f);
+
+                Assert.Less(asteroid.transform.position.y, initialYPos);
+            }
+
+            [UnityTest]
+            public IEnumerator GameOverOccursOnAsteroidCollision()
+            {
+                GameObject asteroid = game.GetSpawner().SpawnAsteroid();
+                asteroid.transform.position = game.GetShip().transform.position;
+                yield return new WaitForSeconds(0.1f);
+
+                Assert.True(game.isGameOver);
+            }
         }
-    }
 }
